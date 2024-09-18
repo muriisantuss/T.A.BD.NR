3a122b5bcb7e40242261f1f3762dbb6fbd890296
muriisantuss <muriisantuss@gmail.com>
2024-09-18 18:33:58 -0300

    :speakly: feat: create login page layout

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, TextInput, SafeAreaView, Pressable, Image } from 'react-native';
+
 
 export default function App() {
   return (
-    <login></login>
+    <Login></Login>
   );
 }
 
-export function login() {
-  const [text, onChangeText] = React.useState('Useless Text');
-  const [number, onChangeNumber] = React.useState('');
+export function Login() {
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <Image 
+      style={styles.logo}
+      source={require('./assets/logo.png')}/>
+      <Text style={styles.title}>MyApp</Text>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        placeholder="Enter your name"
       />
       <TextInput
         style={styles.input}
-        onChangeText={onChangeNumber}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
+        placeholder="Enter your password "
       />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+      <Pressable style={styles.button}>
+        <Text style={styles.textButton}>Log-in</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+
 }
 
 const styles = StyleSheet.create({
@@ -36,4 +39,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    height: 100,
+    width: 100,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  button: {
+    borderRadius: 50,
+    backgroundColor: '#546c04',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    width: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    
+  },
+  textButton: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
