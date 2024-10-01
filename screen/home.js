784ac85f755c3f4fdda4abf628062ce7de30f336
muriisantuss <muriisantuss@gmail.com>
2024-10-01 18:23:20 -0300

    :lipstick: feat: create page home

---
+++ screen/home.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  SafeAreaView,
+  Pressable,
+  Image,
+} from "react-native";
+
+export function Home() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>WelcomeHome</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screen/login.js
@@ -0,0 +1,67 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, SafeAreaView, Pressable, Image } from 'react-native';
+
+
+export function Login() {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image 
+      style={styles.logo}
+      source={require('./../assets/logo.png')}/>
+      <Text style={styles.title}>MyApp</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your password "
+      />
+      <Pressable style={styles.button}>
+        <Text style={styles.textButton}>Log-in</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    height: 100,
+    width: 100,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  button: {
+    borderRadius: 50,
+    backgroundColor: '#546c04',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    width: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    
+  },
+  textButton: {
+    color: 'white',
+    fontWeight: 'bold',
+  }
+});
