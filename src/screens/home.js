135aefd7d13ccc931c0a8a7ee85283938afedd2a
muriisantuss <muriisantuss@gmail.com>
2024-10-04 00:21:55 -0300

    :sparkles: feat: add task to ScrollView component, connected to Firebase for data retrieval.

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,44 +1,71 @@
+import { collection, getFirestore, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
   SafeAreaView,
   Pressable,
-  View
+  View,
+  ScrollView
 } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export function Home({ navigation }) {
+  const [newTask, setNewTask] = useState([])
+  const db = getFirestore()
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const querySnapshot = await getDocs(collection(db, "appTask"))
+    const newTaskArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+    setNewTask(newTaskArray)
+  }
+
+  const goToNewTask = () => {
+    navigation.navigate('NewTask', {
+      onGoBack: fetchTasks,
+    })
+  }
+  const Item = ({ task }) => {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.cardContainer}>
+          <Text style={styles.title}>{task.name} </Text>
+          <Text style={styles.date}>Deadline: {task.dateTask}</Text>
+
+          <View style={styles.buttonContainer}>
+            <Pressable style={[styles.button, { backgroundColor: "#4B53B4" }]}>
+              <Text><Icon name="pencil" size={15} color="#fff" /></Text>
+            </Pressable>
+            <Pressable style={[styles.button, { backgroundColor: "red", marginLeft: 8 }]}>
+              <Text><Icon name="trash" size={15} color="#fff" /></Text>
+            </Pressable>
+          </View>
+        </View>
+      </SafeAreaView>
+    )
+  }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.cardContainer}>
-        <Text style={styles.title}>Task: </Text>
-        <Text style={styles.date}>Deadline:</Text>
-
-        <View style={styles.buttonContainer}>
-          <Pressable style={[styles.button, { backgroundColor: "#4B53B4" }]}>
-            <Text><Icon name="pencil" size={15} color="#fff" /></Text>
-          </Pressable>
-          <Pressable style={[styles.button, { backgroundColor: "red", marginLeft: 8 }]}>
-            <Text><Icon name="trash" size={15} color="#fff" /></Text>
-          </Pressable>
-        </View>
-      </View>
-
-      <SafeAreaView style={styles.footer}>
-        <Pressable onPress={() => navigation.navigate('NewTask')} style={[styles.addButton, { backgroundColor: "#546c04", width: 40, height: 40 }]}>
-          <Text><Icon name="plus" size={15} color="#fff" /></Text>
-        </Pressable>
-      </SafeAreaView>
+    <SafeAreaView style={styles.footer}>
+      <ScrollView style={{ paddingBottom: 60, paddingHorizontal: 16 }}>
+        {newTask.map(task => (
+          <Item key={task.id} task={task} />
+        ))}
+      </ScrollView>
+      <Pressable onPress={goToNewTask} style={[styles.addButton, { backgroundColor: "#546c04", width: 40, height: 40 }]}>
+        <Text><Icon name="plus" size={15} color="#fff" /></Text>
+      </Pressable>
     </SafeAreaView>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
   },
   title: {
--- src/screens/newTask.js
+++ src/screens/newTask.js
@@ -1,20 +1,42 @@
 import { StyleSheet, SafeAreaView, View, Text, TextInput, Pressable } from 'react-native';
+import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { useState } from 'react';
 
-export function NewTask({ navigation }) {
+export function NewTask({ navigation, route }) {
+  const [nameTask, setNameTask] = useState();
+  const [dateTask, setDateTask] = useState();
+
+  const db = getFirestore();
+
+  const handleAddTask = async () => {
+    if (nameTask.trim() == '' || dateTask.trim() == '') {
+      alert("Error", "All fields are required!")
+    }
+    try {
+      await addDoc(collection(db, "appTask"), { name: nameTask, dateTask })
+      alert("Success", "task added successfully")
+      navigation.navigate('Home')
+      route.params?.onGoBack();
+    } catch (error) {
+      Alert.alert("Error", "Failed to add new task " + error.message);
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Add New Task!!</Text>
       <TextInput
         style={styles.input}
         placeholder="Task name: "
+        onChangeText={setNameTask}
       />
       <TextInput
         style={styles.input}
         placeholder="Deadline for completion: "
-
+        onChangeText={setDateTask}
       />
 
-      <Pressable onPress={() => navigation.navigate('Home')} style={styles.button}>
+      <Pressable onPress={handleAddTask} style={styles.button}>
         <Text style={styles.textButton}>Add</Text>
       </Pressable>
       <Pressable onPress={() => navigation.navigate('Home')}>
